c66e3584f6548e9d506ff2237e84c427a9ada925
Muhammed Nabeel Azard <nabeelazard@cmail.carleton.ca>
2023-09-21 15:19:48 -0400

    Added a Buddy object to the ArrayList and then Removed it from the same ArrayList.

--- Lab 1/src/AddressBook.java
+++ Lab 1/src/AddressBook.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
 public class AddressBook {
-
-    public ArrayList<BuddyInfo> all_BuddyInfo;
-    public void addBuddy () {
-
-    }
-
-    public void removeBuddy () {
-
-    }
+    private ArrayList<BuddyInfo> all_buddyInfo = new ArrayList<>();
+    //public ArrayList<BuddyInfo> all_BuddyInfo;
     public static void main(String [] args) {
+
+
         System.out.println("Address Book");
+    }
+
+    public void addBuddy (BuddyInfo aBuddy){
+        all_buddyInfo.add(aBuddy);
+    }
+
+    public void removeBuddy (BuddyInfo aBuddy){
+        all_buddyInfo.remove(aBuddy);
     }
 }
