f0f1577809475b09c28fea637b5cc7cc0f68abcd
Muhammed Nabeel Azard <nabeelazard@cmail.carleton.ca>
2023-09-21 13:20:48 -0400

    Added a print statement that prints the name of the class.

---
+++ Lab 1/src/AddressBook.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class AddressBook {
+
+    public ArrayList<BuddyInfo> all_BuddyInfo;
+    public void addBuddy () {
+
+    }
+
+    public void removeBuddy () {
+
+    }
+
+    public static void main(String [] args) {
+        System.out.println("Address Book");
+    }
+}
